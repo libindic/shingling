fc0329aef4153d95c0771947099ac9ed836042dd
Balasankar C <balasankarc@autistici.org>
2017-09-07 16:50:26 +0530

    Convert to namespace package

--- setup.py
+++ setup.py
@@ -1,25 +1,8 @@
-# coding: utf-8
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
-
-name = "shingling"
+from setuptools import setup
 
 setup(
-    name=name,
-    version="0.1",
-    license="LGPL-3.0",
-    description="A library for making w shingles",
-    author="Santhosh Thottingal",
-    author_email="santhosh.thottingal@gmail.com",
-    long_description="""a wshingling is a set of unique 'shingles'—contiguous
-    subsequences of tokens in a document—that can be used to
-    gauge the similarity of two documents. The w denotes the number
-    of tokens in each shingle in the set.English, Hindi,
-    Malayalam, Kannada, Bengali suported now.""",
-    packages=find_packages(),
-    include_package_data=True,
-    setup_requires=['setuptools-git'],
-    install_requires=['setuptools', 'silpa_common','indicngram'],
-    zip_safe=False,
+    setup_requires=['pbr'],
+    pbr=True,
 )
